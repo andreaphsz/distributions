8168aff2b679136c0c227bceb43fa56953c607a4
Andrea Cantieni <cantieni@eiger.unterrichtsmedien.ch>
2014-01-16 16:21:25 +0100

    init

---
+++ server.R
@@ -0,0 +1,68 @@
+library(shiny)
+library(ggplot2)
+
+# Define server logic for random distribution application
+shinyServer(function(input, output) {
+ 
+  # Reactive expression to generate the requested distribution. This is 
+  # called whenever the inputs change. The output functions defined 
+  # below then all use the value computed from this expression
+  data <- reactive({
+    set.seed(input$seed)
+    dist <- switch(input$dist,
+                   dbeta = rbeta(input$n, input$shape1, input$shape2, input$ncp),
+                   dbinom = rbinom(input$n, input$size, input$prob),
+                   dcauchy = rcauchy(input$n, input$location, input$scale),
+                   dchisq = rchisq(input$n, input$df, input$ncp1),
+                   dexp = rexp(input$n, input$rate),
+                   df = rf(input$n, input$df1, input$df2, input$ncp2),
+                   dgamma = rgamma(input$n, input$shape, input$rate),
+                   dgeom = rgeom(input$n, input$prob),
+                   dhyper = rhyper(input$n, input$m, input$n_, input$k),
+                   dlnorm = rlnorm(input$n, input$meanlog, input$sdlog),
+                   dmultinom = rmultinom(input$n, input$size, input$prob),
+                   dnbinom = rnbinom(input$n, input$size, input$prob, input$mu),
+                   dnorm = rnorm(input$n, input$mean, input$sd),
+                   dpois = rpois(input$n, input$lambda),
+                   dt = rt(input$n, input$df, input$ncp),
+                   dunif = runif(input$n, input$min, input$max),
+                   dweibull = rweibull(input$n, input$shape, input$scale),
+                   dsignrank = rsignrank(input$n, input$n_),
+                   dwilcox = rwilcox(input$n, input$m, input$n_)
+                   )
+    
+  })
+  
+  # Generate a plot of the data. Also uses the inputs to build the 
+  # plot label. Note that the dependencies on both the inputs and
+  # the data reactive expression are both tracked, and all expressions 
+  # are called in the sequence implied by the dependency graph
+  output$plot <- renderPlot({
+    dist <- input$dist
+    n <- input$n
+    
+    #hist(data(), main=paste(dist, '(', n, ')', sep=''))
+    #print(qplot(data(), geom="histogram", main=dist) + geom_density())
+    d <- as.data.frame(data())
+    colnames(d) <- "x"
+    m <- ggplot(d, aes(x))
+    print(m + geom_histogram(aes(y=..density..)) + geom_density(color="blue"))
+  })
+  
+  # Generate a summary of the data
+  output$summary <- renderPrint({
+    list("summary"=summary(data()),
+      "var"=var(data()),
+      "sd"=sd(data()),
+      "se"=sd(data())/sqrt(input$n),
+      "percent inside one sd" = 100*sum(mean(data()) - sd(data()) < data() & data() <  mean(data()) + sd(data()))/input$n,
+     "percent inside two sd" = 100*sum(mean(data()) - 2*sd(data()) < data() & data() <  mean(data()) + 2*sd(data()))/input$n
+    )
+  })
+  
+  # Generate an HTML table view of the data
+  #output$table <- renderTable({
+  #  data.frame(x=data())
+  #})
+  
+})
---
+++ ui.R
@@ -0,0 +1,148 @@
+library(shiny)
+
+# Define UI for random distribution application 
+shinyUI(pageWithSidebar(
+    
+  # Application title
+  headerPanel(HTML("Distributions in the stats package [<a href='http://stat.ethz.ch/R-manual/R-patched/library/stats/html/Distributions.html' target=_blank>R-manual</a>]"), "Distributions in the stats package"),
+  #helpText("http://stat.ethz.ch/R-manual/R-patched/library/stats/html/Distributions.html"), 
+  # Sidebar with controls to select the random distribution type
+  # and number of observations to generate. Note the use of the br()
+  # element to introduce extra vertical spacing
+  sidebarPanel(
+    selectInput("dist", "Choose a distribution:",
+                    list("Beta" = "dbeta", 
+                         "Binomiale" = "dbinom", 
+                         "Cauchy" = "dcauchy",
+                         "Chisquare" = "dchisq",
+                         "Exponential" = "dexp",
+                         "FDist" = "df",
+                         "GammaDist" = "dgamma",
+                         "Geometric" = "dgeom",
+                         "Hypergeometric" = "dhyper",
+                         "Lognormal" = "dlnorm",
+                         "Multinom" = "dmultinom",
+                         "NegBinomial" = "dnbinom",
+                         "Normal" = "dnorm",
+                         "Poisson" = "dpois",
+                         "TDist" = "dt",
+                         "Uniform" = "dunif",
+                         "Weibull" = "dweibull",
+                         "SignRank" = "dsignrank",
+                         "Wilcoxon" = "dwilcox"
+                         )
+               ),
+    br(),
+   
+    sliderInput("seed", "Set seed", value=77, min=7, max=777, step=7),
+
+    sliderInput("n", "n: number of observations", value = 50, min = 10, max = 1000, step=10),
+
+    conditionalPanel(
+      condition = "input.dist == 'dbeta'",
+        sliderInput("shape1", "a: positive parameter of the Beta distribution", value=5, min=0.1, max=10, step=0.1),
+        sliderInput("shape2", "b: positive parameter of the Beta distribution", value=5, min=0.1, max=10, step=0.1),
+        sliderInput("ncp", "ncp: non-centrality parameter", value=0, min=0, max=10)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dbinom'",
+        sliderInput("size", "size: number of trials (zero or more)", value=5, min=0, max=100),
+        sliderInput("prob", "prob: probability of success on each trial", value=0.5, min=0, max=1, step=0.05)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dcauchy'",
+        sliderInput("location", "location: location parameter", value=0, min=0, max=10, step=0.1),
+        sliderInput("scale", "scale: scale parameter", value=1, min=0, max=10, step=0.1)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dchisq'",
+        sliderInput("df", "df: degrees of freedom (non-negative, but can be non-integer)", value=5, min=0.1, max=10, step=0.1),
+        sliderInput("ncp1", "ncp: non-centrality parameter (non-negative)", value=1, min=0, max=10, step=0.1)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dexp'",
+        sliderInput("rate", "rate: [vector of] rates", value=1, min=0.1, max=10, step=0.1)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'df'",
+        sliderInput("df1", "df1: degrees of freedom", value=0, min=0, max=10),
+        sliderInput("df2", "df2: degrees of freedom", value=0, min=0, max=10),
+        sliderInput("ncp2", "ncp: non-centrality parameter", value=0, min=0, max=10)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dgamma'",
+        sliderInput("shape", "shape", value=0, min=0, max=10),
+        sliderInput("rate", "rate", value=1, min=1, max=10)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dgeom'",
+        sliderInput("prob", "prob", value=0, min=0, max=1)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dhyper'",
+        sliderInput("m", "white balls", value=0, min=0, max=10),
+        sliderInput("n_", "black balls", value=1, min=0, max=10),
+        sliderInput("k", "balls drawn", value=1, min=0, max=10)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dlnorm'",
+        sliderInput("meanlog", "meanlog", value=0, min=0, max=10),
+        sliderInput("sdlog", "sdlog", value=1, min=1, max=10)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dmultinom'",
+        sliderInput("size", "size", value=0, min=0, max=10),
+        sliderInput("prob", "prob", value=0, min=0, max=1)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dnbinom'",
+        sliderInput("size", "size", value=0, min=0, max=10),
+        sliderInput("prob", "prob", value=0, min=0, max=10),
+        sliderInput("mu", "mu", value=0, min=0, max=10)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dnorm'",
+        sliderInput("mean", "mean", value=0, min=0, max=10),
+        sliderInput("sd", "sd", value=1, min=0, max=10)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dpois'",
+       sliderInput("lambda", "lambda", value=0, min=0, max=10)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dt'",
+        sliderInput("df", "df", value=0, min=0, max=10),
+        sliderInput("ncp", "ncp", value=0, min=0, max=10)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dunif'",
+        sliderInput("min", "min", value=0, min=0, max=10),
+        sliderInput("max", "max", value=0, min=0, max=10)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dweibull'",
+        sliderInput("shape", "shape", value=0, min=0, max=10),
+        sliderInput("scale", "scale", value=0, min=0, max=10)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dsignrank'",
+        sliderInput("n_", "n", value=0, min=0, max=10)
+    ),
+    conditionalPanel(
+      condition = "input.dist == 'dwilcox'",
+        sliderInput("m", "m", value=0, min=0, max=10),
+        sliderInput("n_", "n", value=0, min=0, max=10)
+    )
+ ),
+
+  # Show a tabset that includes a plot, summary, and table view
+  # of the generated distribution
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Plot", plotOutput("plot")), 
+      tabPanel("Summary", verbatimTextOutput("summary")) 
+      #tabPanel("Table", tableOutput("table"))
+    )
+  )
+))
+
